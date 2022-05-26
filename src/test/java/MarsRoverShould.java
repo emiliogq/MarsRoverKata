0df7940aca703117377cefbadaa25f962c5ecbc6
Emilio Garcia <emilio.garcia@codurance.com>
2022-05-26 11:53:36 +0200

    Move and rotations test split to test only one thing

--- src/test/java/MarsRoverShould.java
+++ src/test/java/MarsRoverShould.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -9,6 +10,13 @@ import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class MarsRoverShould {
 
+    private MarsRover marsRover;
+
+    @BeforeEach
+    void setUp() {
+        marsRover = new MarsRover();
+    }
+
     private static Stream<Arguments> moveNorthInputs() {
         return Stream.of(
                 arguments("M", "0:1:N"),
@@ -17,29 +25,41 @@ public class MarsRoverShould {
                 arguments("MMMMMMMMMM", "0:0:N")
         );
     }
+
     @ParameterizedTest
     @MethodSource("moveNorthInputs")
-    void moveNorth(String command, String expectedPosition) {
-        MarsRover marsRover = new MarsRover();
+    void moveUp(String command, String expectedPosition) {
         Assertions.assertEquals(marsRover.execute(command), expectedPosition);
     }
 
-    private static Stream<Arguments> rotateInputs() {
+    private static Stream<Arguments> rotateRightInputs() {
         return Stream.of(
                 arguments("R", "0:0:E"),
                 arguments("RR", "0:0:S"),
                 arguments("RRR", "0:0:W"),
                 arguments("RRRR", "0:0:N"),
-                arguments("RRRRR", "0:0:E"),
-                arguments("L", "0:0:W"),
-                arguments("LL", "0:0:S")
-
+                arguments("RRRRR", "0:0:E")
                 );
     }
     @ParameterizedTest
-    @MethodSource("rotateInputs")
+    @MethodSource("rotateRightInputs")
     void rotate(String command, String expectedPosition) {
-        MarsRover marsRover = new MarsRover();
+        Assertions.assertEquals(marsRover.execute(command), expectedPosition);
+    }
+
+    private static Stream<Arguments> rotateLeftInputs() {
+        return Stream.of(
+                arguments("L", "0:0:W"),
+                arguments("LL", "0:0:S"),
+                arguments("LLL", "0:0:E"),
+                arguments("LLLL", "0:0:N"),
+                arguments("LLLLL", "0:0:W")
+
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("rotateLeftInputs")
+    void rotateLeft(String command, String expectedPosition) {
         Assertions.assertEquals(marsRover.execute(command), expectedPosition);
     }
 
